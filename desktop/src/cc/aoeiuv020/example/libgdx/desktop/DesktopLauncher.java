7286d54b479e9231f40b7155f783334763219e56
AoEiuV020 <aoeiuv020@gmail.com>
2017-11-26 16:31:47 +0800

    Create empty project

---
+++ desktop/src/cc/aoeiuv020/example/libgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cc.aoeiuv020.example.libgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cc.aoeiuv020.example.libgdx.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
